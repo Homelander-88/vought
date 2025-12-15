a61ae7f4e16ca18cef9670508ab45bb63c610d88
Homelander-88 <homelander890123@gmail.com>
2025-12-15 12:54:48 +0530

    Create homelander.js

---
+++ homelander.js
@@ -0,0 +1,1 @@
+javascript:(()=>{if(window.solverLoaded)return;const s=document.createElement('script');s.src='https://cdn.jsdelivr.net/gh/Homelander-88/vought/homeboy.min.js';document.body.appendChild(s);})();
