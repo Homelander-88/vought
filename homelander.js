3cebc4ad286df23aac42873604983e8d037a7020
Homelander-88 <homelander890123@gmail.com>
2025-12-15 13:40:20 +0530

    Delete homelander.js

--- homelander.js
+++
@@ -1,1 +0,0 @@
-javascript:(()=>{if(window.solverLoaded)return;const s=document.createElement('script');s.src='https://cdn.jsdelivr.net/gh/Homelander-88/vought/homeboy.min.js';document.body.appendChild(s);})();
